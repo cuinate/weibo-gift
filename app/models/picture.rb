4dac579a3e64511a0e09dda2fa36971f65ed5c2c
nate.cui <cuiyingsan@gmail.com>
2010-12-13 22:13:41 +0800

    compress pictrue and add paperclip style

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,11 @@
 class Picture < ActiveRecord::Base
   belongs_to :user
-  has_attached_file :photo, :url => "/system/picture/:id/:style/:filename"
+  has_attached_file :photo, 
+                    :default_style => :thumb,
+                    :styles => {
+                      :thumb => "100x50#",
+                      :card  => "360x360#"
+                    },
+                    :url => "/system/picture/:id/:style/:filename"
+                    
 end
