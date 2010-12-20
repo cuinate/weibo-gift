9b78a96e4d69e670404761224b4d9e800aad99be
nate.cui <cuiyingsan@gmail.com>
2010-12-20 11:22:56 +0800

    roll back from S3 file storage to local storage

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,19 +1,11 @@
 class Template < ActiveRecord::Base
   has_attached_file :pic, 
-                    :storage => :s3,
-                    :s3_credentials =>"#{RAILS_ROOT}/config/s3.yml",
-                    :bucket => 'natecui',
                     :default_style => :thumb,
                     :styles => {
                       :thumb  => "200x150#",
                       :thumb_b => "400x300#"
                     },
-                    :url => "/system/template/:id/:style/:filename",
-                    :path =>"/system/template/:id/:style/:filename"                    
+                    :url => "/system/template/:id/:style/:filename"
   has_attached_file :back, 
-                    :storage => :s3,
-                    :s3_credentials =>"#{RAILS_ROOT}/config/s3.yml",
-                    :bucket => 'natecui',
-                    :url => "/system/template/:id/back/:filename",
-                    :path =>"/system/template/:id/:style/:filename"                  
+                    :url => "/system/template/:id/back/:filename"                  
 end
