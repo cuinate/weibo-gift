618cc0b536c51e64b017e954da8b8702b13511b1
nate.cui <cuiyingsan@gmail.com>
2010-12-20 10:10:00 +0800

    add amazon S3 file storage support

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,13 +1,17 @@
 class Picture < ActiveRecord::Base
   belongs_to :user
   has_attached_file :photo, 
-                    :default_style => :thumb,
+                    :storage => :s3,
+                    :s3_credentials => "#{RAILS_ROOT}/config/s3.yml", 
+                    :bucket => 'natecui',
+                    :default_style => :card280,
                     :styles => {
                       :card480  => "480x360#",
                       :card400  => "400x300#",                      
                       :card280  => "280x210#",                    
                     },
-                    :url => "/system/picture/:id/:style/:filename"
+                    :url => "/system/picture/:id/:style/:filename",
+                    :path =>"/system/picture/:id/:style/:filename"
   scope :is_card, where(:is_card => '1')
                     
 end
